085dad2237729bbcc10dbe1cc1519c0b66eccb31
Miracle <nnabukomiracle@gmail.com>
2024-08-01 20:06:31 +0100

    updated deploy.sh

--- introtodevops/deploy.sh
+++ introtodevops/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo /home/ubuntu/.nvm/versions/node/v20.5.1/bin/npm ci
+# sudo /home/ubuntu/.nvm/versions/node/v20.5.1/bin/npm ci
 sudo /home/ubuntu/.nvm/versions/node/v20.5.1/bin/npm run build
-pm2 start npm --name "introtodevops" -- start 
-pm2 restart introtodevops
+/home/ubuntu/.nvm/versions/node/v20.5.1/bin/pm2 start npm --name "introtodevops" -- start 
+/home/ubuntu/.nvm/versions/node/v20.5.1/bin/pm2 restart introtodevops
 sudo systemctl restart nginx
