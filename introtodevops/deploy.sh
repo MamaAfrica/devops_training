013a325dbf8582091081bfe76704be5b32417804
Miracle <nnabukomiracle@gmail.com>
2024-08-01 20:20:52 +0100

    updated deploy.sh

--- introtodevops/deploy.sh
+++ introtodevops/deploy.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 # sudo /home/ubuntu/.nvm/versions/node/v20.5.1/bin/npm ci
-sudo /home/ubuntu/.nvm/versions/node/v20.5.1/bin/npm run build
-/home/ubuntu/.nvm/versions/node/v20.5.1/bin/pm2 start npm --name "introtodevops" -- start 
-/home/ubuntu/.nvm/versions/node/v20.5.1/bin/pm2 restart introtodevops
+
+# Define the path to npm and pm2
+NPM_PATH="/home/ubuntu/.nvm/versions/node/v20.5.1/bin/npm"
+PM2_PATH="/home/ubuntu/.nvm/versions/node/v20.5.1/bin/pm2"
+
+# Run build
+sudo $NPM_PATH run build
+
+# Start or restart the application with PM2
+$PM2_PATH start $NPM_PATH --name "introtodevops" -- start
+
+# Restart nginx
 sudo systemctl restart nginx
