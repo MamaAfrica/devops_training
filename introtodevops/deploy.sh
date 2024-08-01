694d6783f4e64f083d70d7345a769f50c30faf88
Miracle <nnabukomiracle@gmail.com>
2024-08-01 19:35:33 +0100

    updated deploy.sh

--- introtodevops/deploy.sh
+++ introtodevops/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# sudo /home/ubuntu/.nvm/versions/node/v20.5.1/bin/npm install
+sudo /home/ubuntu/.nvm/versions/node/v20.5.1/bin/npm install
 sudo /home/ubuntu/.nvm/versions/node/v20.5.1/bin/npm run build
 pm2 start npm --name "introtodevops" -- start 
 pm2 restart introtodevops
